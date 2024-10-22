06ae5e3fc5ef4d82cd7cd33052bcaf5d1fae191c
lucio.dellafelice <lucio.dellafelice@origins-digital.com>
2024-10-22 13:49:54 +0200

    secret number(fin)

--- app.js
+++ app.js
@@ -26,71 +26,71 @@ Difficile -> Choix nombre entre 1 et 100. 3 essais
 Afficher à la fin du jeu tous les nombres essayés par l'utilisateur */
 //**********  EX2 **********/
 
-dificulty= prompt("enter the dificulty mode you wanna play in(easy/medium/hard)").toLowerCase()
+// dificulty= prompt("enter the dificulty mode you wanna play in(easy/medium/hard)").toLowerCase()
 
-if (dificulty !== 'easy' && dificulty !== 'medium' && dificulty !== 'hard') {
-    dificulty= prompt("wrong input, please enter a choice between(easy/medium/hard)").toLowerCase()
-}
-if (dificulty == 'easy') {
-    easyMode();
-}
-if (dificulty == 'medium') {
-    mediumMode();
-}
-if (dificulty == 'hard') {
-    hardMode();
-}
+// if (dificulty !== 'easy' && dificulty !== 'medium' && dificulty !== 'hard') {
+//     dificulty= prompt("wrong input, please enter a choice between(easy/medium/hard)").toLowerCase()
+// }
+// if (dificulty == 'easy') {
+//     easyMode();
+// }
+// if (dificulty == 'medium') {
+//     mediumMode();
+// }
+// if (dificulty == 'hard') {
+//     hardMode();
+// }
 
-function easyMode() {
-    const secretNumber = Math.floor(Math.random() * 20) + 1;
-    nbTry=0;
-    while (nbTry < 8) {
-        numberTried = parseInt(prompt('enter the secret number'));
-        if (numberTried === secretNumber) {
-            console.log('Congratulations! you guessed correctly the secret number');
-            return;
-        } else {
-            nbTry++;
-            console.log('Incorrect! keep trying, you have '+ (8-nbTry) + ' guesses left');
-        }
-    }
-    console.log('Sorry, you are out of guesses. The secret number was: ' + secretNumber);
+// function easyMode() {
+//     const secretNumber = Math.floor(Math.random() * 20) + 1;
+//     nbTry=0;
+//     while (nbTry < 8) {
+//         numberTried = parseInt(prompt('enter the secret number'));
+//         if (numberTried === secretNumber) {
+//             console.log('Congratulations! you guessed correctly the secret number');
+//             return;
+//         } else {
+//             nbTry++;
+//             console.log('Incorrect! keep trying, you have '+ (8-nbTry) + ' guesses left');
+//         }
+//     }
+//     console.log('Sorry, you are out of guesses. The secret number was: ' + secretNumber);
 
-}
+// }
 
-function mediumMode() {
-    const secretNumber = Math.floor(Math.random() * 50) + 1;
-    nbTry=0;
-    while (nbTry < 5) {
-        numberTried = parseInt(prompt('enter the secret number'));
-        if (numberTried === secretNumber) {
-            console.log('Congratulations! you guessed correctly the secret number');
-            return;
-        } else {
-            nbTry++;
-            console.log('Incorrect! keep trying, you have '+ (5-nbTry) + ' guesses left');
-        }
-    }
-    console.log('Sorry, you are out of guesses. The secret number was: ' + secretNumber);
+// function mediumMode() {
+//     const secretNumber = Math.floor(Math.random() * 50) + 1;
+//     nbTry=0;
+//     while (nbTry < 5) {
+//         numberTried = parseInt(prompt('enter the secret number'));
+//         if (numberTried === secretNumber) {
+//             console.log('Congratulations! you guessed correctly the secret number');
+//             return;
+//         } else {
+//             nbTry++;
+//             console.log('Incorrect! keep trying, you have '+ (5-nbTry) + ' guesses left');
+//         }
+//     }
+//     console.log('Sorry, you are out of guesses. The secret number was: ' + secretNumber);
 
-}
+// }
 
-function hardMode() {
-    const secretNumber = Math.floor(Math.random() * 100) + 1;
-    nbTry=0;
-    while (nbTry < 3) {
-        numberTried = parseInt(prompt('enter the secret number'));
-        if (numberTried === secretNumber) {
-            console.log('Congratulations! you guessed correctly the secret number');
-            return;
-        } else {
-            nbTry++;
-            console.log('Incorrect! keep trying, you have '+ (3-nbTry) + ' guesses left');
-        }
-    }
-    console.log('Sorry, you are out of guesses. The secret number was: ' + secretNumber);
+// function hardMode() {
+//     const secretNumber = Math.floor(Math.random() * 100) + 1;
+//     nbTry=0;
+//     while (nbTry < 3) {
+//         numberTried = parseInt(prompt('enter the secret number'));
+//         if (numberTried === secretNumber) {
+//             console.log('Congratulations! you guessed correctly the secret number');
+//             return;
+//         } else {
+//             nbTry++;
+//             console.log('Incorrect! keep trying, you have '+ (3-nbTry) + ' guesses left');
+//         }
+//     }
+//     console.log('Sorry, you are out of guesses. The secret number was: ' + secretNumber);
 
-}
+// }
 
 
 /* Consignes nombre mystère (fin):
@@ -101,68 +101,69 @@ Afficher aussi au fur et à mesure la liste des essais de mon utilisateur */
 
 /******** EX3 ********/
 
-dificulty= prompt("enter the dificulty mode you wanna play in(easy/medium/hard)").toLowerCase()
+let dificulty= prompt("enter the dificulty mode you wanna play in(easy/medium/hard)").toLowerCase()
 
-if (dificulty !== 'easy' && dificulty !== 'medium' && dificulty !== 'hard') {
+while (dificulty !== 'easy' && dificulty !== 'medium' && dificulty !== 'hard') {
     dificulty= prompt("wrong input, please enter a choice between(easy/medium/hard)").toLowerCase()
 }
-if (dificulty == 'easy') {
+
+document.getElementById('number-form').style.display='block';
+
+let secretNumber;
+let maxTries;
+let nbTry = 0;
+
+if (dificulty === 'easy') {
     easyMode();
-}
-if (dificulty == 'medium') {
+} else if (dificulty === 'medium') {
     mediumMode();
-}
-if (dificulty == 'hard') {
+} else if (dificulty === 'hard') {
     hardMode();
 }
 
 function easyMode() {
-    const secretNumber = Math.floor(Math.random() * 20) + 1;
-    nbTry=0;
-    while (nbTry < 8) {
-        numberTried = parseInt(prompt('enter the secret number'));
-        if (numberTried === secretNumber) {
-            console.log('Congratulations! you guessed correctly the secret number');
-            return;
+    secretNumber = Math.floor(Math.random() * 20) + 1;
+    maxTries = 8;
+    playGame();
+  }
+  
+  function mediumMode() {
+    secretNumber = Math.floor(Math.random() * 50) + 1;
+    maxTries = 5;
+    playGame();
+  }
+  
+  function hardMode() {
+    secretNumber = Math.floor(Math.random() * 100) + 1;
+    maxTries = 3;
+    playGame();
+  }
+
+  function playGame() {
+    const numberForm = document.getElementById("number-form");
+    const resultMessage = document.getElementById("result-message");
+  
+    numberForm.addEventListener("submit", function (event) {
+      event.preventDefault(); 
+  
+      const numberTried = parseInt(document.getElementById("inserted-number").value);
+  
+      if (numberTried === secretNumber) {
+        resultMessage.textContent = "Congratulations! You guessed the correct number.";
+      } else {
+        nbTry++;
+        if (nbTry < maxTries) {
+          resultMessage.textContent =
+            numberTried > secretNumber
+              ? `Incorrect! The secret number is smaller. You have ${maxTries - nbTry} guesses left.`
+              : `Incorrect! The secret number is larger. You have ${maxTries - nbTry} guesses left.`;
         } else {
-            nbTry++;
-            console.log('Incorrect! keep trying, you have '+ (8-nbTry) + ' guesses left');
+          resultMessage.textContent = `Sorry, you are out of guesses. The secret number was: ${secretNumber}.`;
         }
-    }
-    console.log('Sorry, you are out of guesses. The secret number was: ' + secretNumber);
+      }
+      document.getElementById("inserted-number").value = "";
+    });
+  }
 
-}
 
-function mediumMode() {
-    const secretNumber = Math.floor(Math.random() * 50) + 1;
-    nbTry=0;
-    while (nbTry < 5) {
-        numberTried = parseInt(prompt('enter the secret number'));
-        if (numberTried === secretNumber) {
-            console.log('Congratulations! you guessed correctly the secret number');
-            return;
-        } else {
-            nbTry++;
-            console.log('Incorrect! keep trying, you have '+ (5-nbTry) + ' guesses left');
-        }
-    }
-    console.log('Sorry, you are out of guesses. The secret number was: ' + secretNumber);
 
-}
-
-function hardMode() {
-    const secretNumber = Math.floor(Math.random() * 100) + 1;
-    nbTry=0;
-    while (nbTry < 3) {
-        numberTried = parseInt(prompt('enter the secret number'));
-        if (numberTried === secretNumber) {
-            console.log('Congratulations! you guessed correctly the secret number');
-            return;
-        } else {
-            nbTry++;
-            console.log('Incorrect! keep trying, you have '+ (3-nbTry) + ' guesses left');
-        }
-    }
-    console.log('Sorry, you are out of guesses. The secret number was: ' + secretNumber);
-
-}
